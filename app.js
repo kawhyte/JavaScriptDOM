374e2ae7c513f87d2841cbc198e3a62cc6342477
Kenny Kenren Rene <kenren@Kennys-MacBook-Pro.local>
2019-10-10 20:47:19 -0700

    querySelector

---
+++ app.js
@@ -0,0 +1,9 @@
+// const titles  = document.getElementsByClassName('title');
+
+// Array.from(titles).forEach((element) => {
+//     console.log(element.innerHTML);
+// });
+
+const wrapper = document.querySelector('#book-list li:nth-child(2) .name')
+
+console.log(wrapper)
