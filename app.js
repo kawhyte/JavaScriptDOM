a355089a31f8832e31d8ad00f38e2790a451b941
Kenny Kenren Rene <kenren@kennys-mbp.attlocal.net>
2019-10-11 21:36:24 -0700

    querySelectorAll

--- app.js
+++ app.js
@@ -4,6 +4,12 @@
 //     console.log(element.innerHTML);
 // });
 
-const wrapper = document.querySelector('#book-list li:nth-child(2) .name')
+//const wrapper = document.querySelector('#book-list li:nth-child(2) .name')
+const wrapper = document.querySelectorAll('#book-list  li .name')
 
-console.log(wrapper)
+
+Array.from(wrapper).forEach(element =>{
+
+    console.log(element)
+
+})
