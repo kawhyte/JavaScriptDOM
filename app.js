2c463ba8aa391809d7ea82624fa9af017653da15
Kenny Kenren Rene <kenren@kennys-mbp.attlocal.net>
2019-10-11 21:49:53 -0700

    Update InnerHTML

--- app.js
+++ app.js
@@ -7,9 +7,14 @@
 //const wrapper = document.querySelector('#book-list li:nth-child(2) .name')
 const wrapper = document.querySelectorAll('#book-list  li .name')
 
+wrapper.forEach(element =>{
 
-Array.from(wrapper).forEach(element =>{
-
-    console.log(element)
+    console.log(element.innerHTML)
+    element.innerHTML += " Kenny"
 
 })
+
+const bookList = document.querySelector('#book-list')
+console.log (bookList.innerHTML)
+bookList.innerHTML +=`<h1> This is new</h2>
+<p> Para1</p>`
